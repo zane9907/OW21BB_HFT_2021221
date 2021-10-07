3defac17275ad814f9e4130be8bfd75de452207b
Botond Antalóczy <zane9907@gmail.com>
2021-10-07 13:16:54 +0200

    .mdf és .ldf files are created. Empty projects created.

--- OW21BB_HFT_2021221.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace OW21BB_HFT_2021221.Data
-{
-    public class Class1
-    {
-    }
-}
--- OW21BB_HFT_2021221.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace OW21BB_HFT_2021221.Models
-{
-    public class Class1
-    {
-    }
-}
