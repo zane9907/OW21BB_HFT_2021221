dc2adf02d25db09144e801218cc306bc8a9fc34d
Botond Antalóczy <zane9907@gmail.com>
2021-10-14 16:01:10 +0200

    3 tables are crated: Doctor, Hospital, Patient

---
+++ OW21BB_HFT_2021221.Models/Doctor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OW21BB_HFT_2021221.Models
+{
+    [Table("Doctor")]
+    public class Doctor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DoctorID { get; set; }
+
+        [ForeignKey(nameof(Hospital))]
+        public int HospitalID { get; set; }
+
+        //[ForeignKey(nameof(Patient))]        
+        //public int SocialSecurityNumber { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        public string Specialization { get; set; }
+
+        public string AllData => $"[{DoctorID}] -> {Name} - {Specialization} - Number of Patients: {Patients.Count()}";
+
+        [NotMapped]
+        public virtual Hospital Hospital { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Patient> Patients { get; set; }
+
+        public Doctor()
+        {
+            this.Patients = new HashSet<Patient>();
+        }
+
+    }
+}
---
+++ OW21BB_HFT_2021221.Models/Hospital.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OW21BB_HFT_2021221.Models
+{
+    [Table("Hospital")]
+    public class Hospital
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int HospitalID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        //[ForeignKey(nameof(Doctor))]        
+        //public int DoctorID { get; set; }
+
+        public string Location { get; set; }
+
+        [NotMapped]
+        public string AllData => $"[{HospitalID}] -> {Name} - {Location} - Number of Doctors: {Doctors.Count()}";
+
+        [NotMapped]
+        public virtual ICollection<Doctor> Doctors { get; set; }
+
+        public Hospital()
+        {
+            this.Doctors = new HashSet<Doctor>();
+        }
+    }
+}
