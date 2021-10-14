af62dcf5867a36171130991e9e5423058107bbf1
Botond Antalóczy <zane9907@gmail.com>
2021-10-14 15:53:25 +0200

    .mdf and .ldf files  renamed from Database1 to HospitalDatabase for optimal usage.
    3 tables are created: Patient, Doctor, Hospital.
    And they have content.

---
+++ OW21BB_HFT_2021221.Models/Patient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OW21BB_HFT_2021221.Models
+{
+    [Table("Patient")]
+    public class Patient
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //[MaxLength(9)]
+        //[Required]
+        public int SocialSecurityNumber { get; set; }
+
+        [ForeignKey(nameof(Doctor))]
+        public int DoctorID { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        public int? Age { get; set; }
+
+        public string Address { get; set; }
+
+        public string Alldata => $"[{SocialSecurityNumber}] -> {Name} - {Age} - {Address}";
+
+        [NotMapped]
+        public virtual Doctor Doctor { get; set; }
+    }
+}
