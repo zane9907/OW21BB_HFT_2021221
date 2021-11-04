3618d771a9fa37dc6f8330e3bf2ae195112caea0
Botond Antalóczy <zane9907@gmail.com>
2021-11-04 16:09:40 +0100

    PatientRepository.cs is created. CRUD methods exist.

---
+++ OW21BB_HFT_2021221.Repository/PatientRepository.cs
@@ -0,0 +1,42 @@
+﻿using OW21BB_HFT_2021221.Data;
+using OW21BB_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OW21BB_HFT_2021221.Repository
+{
+    public class PatientRepository : Repository<Patient>
+    {
+        public PatientRepository(HospitalDbContext hpctx) : base(hpctx)
+        {
+        }
+
+        public override void Create(Patient t)
+        {
+            hpctx.Patients.Add(t);
+            hpctx.SaveChanges();
+        }
+
+        public override void Delete(Patient t)
+        {
+            var patientDelete = Read(t.PatientID);
+            hpctx.Patients.Remove(patientDelete);
+            hpctx.SaveChanges();
+        }
+
+        public override Patient Read(int id)
+        {
+            return ReadAll().SingleOrDefault(x => x.PatientID.Equals(id));
+        }
+
+        public override void Update(Patient t)
+        {
+            var patientUpdate = Read(t.PatientID);
+            patientUpdate.Name = t.Name;
+            hpctx.SaveChanges();
+        }
+    }
+}
