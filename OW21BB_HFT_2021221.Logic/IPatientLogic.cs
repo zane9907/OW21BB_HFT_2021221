9d7cfef770467e169e667f59630fa686f1871f5e
Botond Antalóczy <zane9907@gmail.com>
2021-11-18 16:04:41 +0100

    IPatientLogic interface is created. Only basic CRUD methods exist.

---
+++ OW21BB_HFT_2021221.Logic/IPatientLogic.cs
@@ -0,0 +1,20 @@
+﻿using OW21BB_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OW21BB_HFT_2021221.Logic
+{
+    public interface IPatientLogic
+    {
+        Patient GetHospitalById(int id);
+        IEnumerable<Patient> GetAllBlogs();
+        void AddNewHospital(Patient patient);
+        void UpdateHospital(Patient patient);
+        void DeleteHospital(Patient patient);
+
+        //TODO NON-CRUD
+    }
+}
---
+++ OW21BB_HFT_2021221.Logic/PatientLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OW21BB_HFT_2021221.Logic
+{
+    public class PatientLogic : IPatientLogic
+    {
+    }
+}
