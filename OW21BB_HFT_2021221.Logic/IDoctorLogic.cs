e84cf9b9293222fc7691215f28616867eda2abce
Botond Antalóczy <zane9907@gmail.com>
2021-11-18 15:30:31 +0100

    IDoctorLogic interface is created. Only basic CRUD methods exist.

---
+++ OW21BB_HFT_2021221.Logic/IDoctorLogic.cs
@@ -0,0 +1,20 @@
+﻿using OW21BB_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OW21BB_HFT_2021221.Logic
+{
+    public interface IDoctorLogic
+    {
+        Doctor GetHospitalById(int id);
+        IEnumerable<Hospital> GetAllBlogs();
+        void AddNewHospital(Hospital hospital);
+        void UpdateHospital(Hospital hospital);
+        void DeleteHospital(Hospital id);
+
+        //TODO NON-CRUD
+    }
+}
